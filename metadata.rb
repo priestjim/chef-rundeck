1b84e69a432bf4a8ee730cbf90b3eaad3ba99a44
Panagiotis Papadomitsos <pj@ezgr.net>
2012-11-07 14:27:33 +0200

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Metaworx Inc."
+maintainer_email "pj@ezgr.net"
+license          ""
+description      "Installs/Configures chef-rundeck"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: chef-rundeck
+# Recipe:: default
+#
+# Copyright 2012, Metaworx Inc.
+#
