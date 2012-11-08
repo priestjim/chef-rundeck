658613ae30e6922f40ca0917c8e5314e8f6433e6
Panagiotis Papadomitsos <pj@ezgr.net>
2012-11-08 22:20:03 +0200

    metadata update

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,10 @@ description      "Installs/Configures chef-rundeck"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+%{ ubuntu debian }.each do |system|
+	supports system
+end
+
 depends 'java'
-suggests 'nginx'
+recommends 'nginx'
+recommends 'supervisor'
